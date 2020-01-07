56258c56f7393f02c1a64750c72ff779425f3996
Michael Hopcroft <mhop@microsoft.com>
2020-01-07 09:41:56 -0800

    naming.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './cli';
 export * from './cloud';
 export * from './laboratory';
+export * from './naming';
 export * from './secrets';
 export * from './shell';
 export * from './utilities';
---
+++ src/naming/naming.ts
@@ -0,0 +1,85 @@
+const base32 = require('base32') as IBase32;
+
+// tslint:disable-next-line:interface-name
+interface IBase32 {
+    encode(text:string): string;
+    decode(text:string): string;
+}
+
+const benchmarks = 'benchmarks';
+const candidates = 'candidates';
+const runs = 'runs';
+const suites = 'suites';
+
+export function encodeBenchmark(name: string): string {
+    return encode(benchmarks, name)
+}
+
+export function decodeBenchmark(encoded: string): string {
+    return decode(benchmarks, encoded);
+}
+
+export function encodeCandidate(name: string): string {
+    return encode(candidates, name)
+}
+
+export function decodeCandidate(encoded: string): string {
+    return decode(candidates, encoded);
+}
+
+export function encodeSuite(name: string): string {
+    return encode(suites, name)
+}
+
+export function decodeSuite(encoded: string): string {
+    return decode(suites, encoded);
+}
+
+// NOTE: run encoding differs from benchmarks, candidates, and suites.
+export function encodeRun(uid: string): string {
+    return `/${runs}/${uid}`
+}
+
+// NOTE: run encoding differs from benchmarks, candidates, and suites.
+export function decodeRun(encoded: string): string {
+    const parts = encoded.split('/');
+    if (parts.length === 3) {
+        return parts[1];
+    } else {
+        const message = `decode: invalid encoding "${encoded}"`;
+        throw TypeError(message);
+    }
+}
+
+function encode(collection: string, name: string) {
+    return `/${collection}/${base32.encode(name)}`;
+}
+
+function decode(collection: string, encoded: string) {
+    const parts = encoded.split('/');
+    if (parts.length === 3) {
+        if (parts[1] === collection) {
+            return base32.decode(parts[2]);
+        } else {
+            const message = `decode: collection mismatch - found "${parts[1]} - expected ${collection}"`;
+            throw TypeError(message);
+        }
+    } else {
+        const message = `decode: invalid encoding "${encoded}"`;
+        throw TypeError(message);
+    }
+}
+
+// Save for unit tests
+// console.log(encodeBenchmark('foobar'));
+// console.log(decodeBenchmark(encodeBenchmark('foobar')));
+
+// console.log(encodeCandidate('foobar'));
+// console.log(encodeCandidate(encodeCandidate('foobar')));
+
+// console.log(encodeSuite('foobar'));
+// console.log(decodeSuite(encodeSuite('foobar')));
+
+// console.log(encodeRun('foobar'));
+// console.log(decodeRun(encodeRun('foobar')));
+
